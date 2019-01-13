f29622352d92f371c27d5a1cdd84373c506c8e59
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-01-13 09:40:15 +0100

    Changes expectedPgnWithoutBlunders => tests passes

--- test/index.js
+++ test/index.js
@@ -7,7 +7,7 @@ const path = resolve(__dirname, '../bin/chess-steg')
 // with blunders
 const testString = 'hello world'
 const expectedPgn = '1. e3 Nh6 2. d4 Nf5 3. Qd2 Rg8 4. Ne2 c6 5. b3 d6 6. a3 Be6 7. d5 Kd7 8. Qd3 Kc8 9. Qb5 b6 10. Ng1 { Black resigns. } 1-0'
-const expectedPgnWithoutBlunders = '1. Nf3 g6 2. e3 Bh6 3. a3 d6 4. Bb5+ Kf8 5. Ng1 e5 6. f3 c5 7. a4 Qc7 8. Kf2 a6 9. Bc4 Nc6 10. e4 Ke7 11. d3 Qd7 12. Ke1 Bg7 13. f4 Nb8 { White resigns. } 0-1'
+const expectedPgnWithoutBlunders = '1. g3 h5 2. a4 b6 3. f3 d6 4. Bg2 c6 5. e3 e5 6. Kf1 Qe7 7. b3 Ba6+ 8. Ke1 h4 9. Bf1 Bb7 10. a5 hxg3 11. h3 b5 12. Qe2 Qh4 13. Qg2 d5 14. Bb2 Qg5 15. Ra2 { Black resigns. } 1-0'
 
 test('basic check', t => {
   t.true(true, 'ava works ok')
